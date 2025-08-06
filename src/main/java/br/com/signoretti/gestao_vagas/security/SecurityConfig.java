1ead22f448c490b81da048eadb591fc171bf0572
Sign0retti <122103643+Sign0retti@users.noreply.github.com>
2025-08-06 16:38:36 -0300

    feat: Spring Security

---
+++ src/main/java/br/com/signoretti/gestao_vagas/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package br.com.signoretti.gestao_vagas.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    
+    @Bean
+    //Bean é basicamente a sobrescrita do método na camada do método original (faça isso aqui ao inves da sua ação original).
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> {
+            auth.requestMatchers("/candidate/").permitAll().requestMatchers("/company/").permitAll();
+            auth.anyRequest().authenticated();
+        })
+        
+        ;
+
+
+
+        return http.build();
+    }
+
+}
